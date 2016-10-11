09530e80c135b75ef85f81585e69fad6902fa89e
lance <lance36@gmail.com>
2016-10-11 21:36:05 +0200

    reindent

--- script.sh
+++ script.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
 HEIGHT=15
 WIDTH=70
 CHOICE_HEIGHT=4
 BACKTITLE="NetApp Metrocluster Switchover Control"
 TITLE="Switchover Control"
 MENU="Choose one of the following options:"
-
 OPTIONS=(1 "Switchover PIANIFICATO (Manutenzione)"
          2 "Switchover FORZATO (Disastro)")
-
 CHOICE=$(dialog --no-lines\
                 --clear \
                 --backtitle "$BACKTITLE" \
@@ -18,7 +15,6 @@ CHOICE=$(dialog --no-lines\
                 $HEIGHT $WIDTH $CHOICE_HEIGHT \
                 "${OPTIONS[@]}" \
                 2>&1 >/dev/tty)
-
 case $CHOICE in
         1) #PIANIFICATO
                 TITLE="Switchover PIANIFICATO"
@@ -33,19 +29,20 @@ case $CHOICE in
                 "${OPTIONS[@]}" \
                 2>&1 >/dev/tty)
                 case $CHOICE2 in
-                        1)
+                        1) 
 							clear
 							./spegni_cb_pianificato.sh
 							exit 0
                         ;;
-                        2)
+                        2) 
 							clear
 							./spegni_sp_pianificato.sh
 							exit 0
                         ;;
 		esac
         ;;
-        2) #FORZATO
+        2)
+            #FORZATO
 			dialog --no-lines --title "[ ! ] ATTENZIONE[ ! ]" --yesno "inserire warning-text qui" 13 125 
 			if [[ $? == 1 ]]; then echo "Operazione Annullata";exit $rc; fi
                 TITLE="[ ! ] Switchover FORZATO [ ! ]"
@@ -60,17 +57,16 @@ case $CHOICE in
                 "${OPTIONS[@]}" \
                 2>&1 >/dev/tty)
                 case $CHOICE2 in
-                        1)
+                        1) 
 							clear
-							./spegni_cb_forzato.sh
+							./spegni_cb_pianificato.sh
 							exit 0
                         ;;
-                        2)
+                        2) 
 							clear
-							./spegni_sp_forzato.sh
+							./spegni_sp_pianificato.sh
 							exit 0
                         ;;
 		esac
             ;;
-
 esac
